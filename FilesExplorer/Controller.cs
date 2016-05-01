e99fd3625c540ffaa2b7d4e559a0cf9d044a0948
AlexanderKartashov <ieronimbosh@gmail.com>
2016-05-01 21:16:45 +0300

    добавлен каркас оконного приложения добавлено построение модели в фоновом потоке с  прогрессом

---
+++ FilesExplorer/Controller.cs
@@ -0,0 +1,84 @@
+﻿using Model.Core;
+using Model.Processing;
+using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace FilesExplorer
+{
+	public class Controller
+	{
+		public event Action ModelCreationCompleteEvent;
+		public event Action<String> ModelCreationProgressEvent;
+
+		public String RootPath { get; set; }
+
+		class Result
+		{
+			public IFileSystemItem Model { get; set; }
+			public String ErrorMessage { get; set; }
+		}
+
+		private BackgroundWorker _bw = new BackgroundWorker();
+		private ItemsProcessor _itemsProcessor = new ItemsProcessor();
+
+		public Controller()
+		{
+			_bw.DoWork += _bw_DoWork;
+			_bw.RunWorkerCompleted += _bw_RunWorkerCompleted;
+			_bw.ProgressChanged += _bw_ProgressChanged;
+			_bw.WorkerReportsProgress = true;
+		}
+
+		public void CreateModel()
+		{
+			_bw.RunWorkerAsync(RootPath);
+		}
+
+		private void _bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			ModelCreationProgressEvent?.Invoke(e.UserState as String);
+		}
+
+		private void _bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			ModelCreationCompleteEvent?.Invoke();
+
+			var result = e.Result as Result;
+			if (result.Model == null)
+			{
+				MessageBox.Show(result.ErrorMessage);
+			}
+			else
+			{
+				// todo
+			}
+		}
+
+		private void _bw_DoWork(object sender, DoWorkEventArgs e)
+		{
+			String path = (String)e.Argument;
+			var bw = sender as BackgroundWorker;
+			IFileSystemItem model = null;
+
+			try
+			{
+				var _modelCreator = new ModelCreator();
+				_modelCreator.ItemAddedInModelEvent += (String itemPath, bool isFolder) => {
+					if (isFolder)
+					{
+						bw.ReportProgress(0, itemPath);
+					}
+				};
+				model = _modelCreator.GetFileSystemIerarchy(path);
+			}
+			catch (Exception ex)
+			{
+				e.Result = new Result { Model = null, ErrorMessage = ex.Message };
+				return;
+			}
+
+			e.Result = new Result { Model = model, ErrorMessage = null };
+		}
+	}
+}
---
+++ FilesExplorer/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilesExplorer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private Controller _controller = new Controller();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			InitBindings();
+		}
+
+		private void InitBindings()
+		{
+			rootFolderText.DataContext = _controller;
+
+			_controller.ModelCreationCompleteEvent += _controller_ModelCreationCompleteEvent;
+			_controller.ModelCreationProgressEvent += _controller_ModelCreationProgressEvent;
+		}
+
+		private void _controller_ModelCreationProgressEvent(string obj)
+		{
+			progressDescription.Text = String.Format("Processing folder {0}", obj);
+		}
+
+		private void _controller_ModelCreationCompleteEvent()
+		{
+			progressLayer.Visibility = Visibility.Hidden;
+		}
+
+		private void viewButton_onClick(object sender, RoutedEventArgs e)
+		{
+			progressLayer.Visibility = Visibility.Visible;
+			_controller.CreateModel();
+		}
+	}
+}
